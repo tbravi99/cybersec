9165dac2609298586a973a2e158b7ed67d8f753a
{ Tommi_Bravi } <tommasobravi9@gmail.com>
2026-02-02 09:36:50 +0100

    Implement file reading and error handling
    Add file reading functionality with error handling

--- leggo.py
+++ leggo.py
@@ -1,1 +1,8 @@
-# non mi ricordo assolutamente leggo.py
+nomefile = "logfile.txt"
+try
+  f = open(nomefile; "r", encoding = "utf-8")
+  righe = f.readlines()
+  for riga in righe: 
+      print(riga, end = "")
+except FileNotFoundError as e:
+  print(f"[-] errore bloccante: {str(e)}")
