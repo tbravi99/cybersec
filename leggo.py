f1afb454e7902a91a50fc683f08470ab2b69eb79
{ Tommi_Bravi } <tommasobravi9@gmail.com>
2026-02-02 09:44:53 +0100

    Fix indentation and syntax in file handling

--- leggo.py
+++ leggo.py
@@ -1,8 +1,8 @@
 nomefile = "logfile.txt"
-try
-  f = open(nomefile; "r", encoding = "utf-8")
-  righe = f.readlines()
-  for riga in righe: 
-      print(riga, end = "")
+try:
+    f = open(nomefile; "r", encoding = "utf-8")
+    righe = f.readlines()
+    for riga in righe: 
+        print(riga, end = "")
 except FileNotFoundError as e:
-  print(f"[-] errore bloccante: {str(e)}")
+    print(f"[-] errore bloccante: {str(e)}")
