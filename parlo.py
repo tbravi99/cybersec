789c3a426520ad7ffc6cb0fa483916b8404bb637
{ Tommi_Bravi } <tommasobravi9@gmail.com>
2026-02-09 10:14:53 +0100

    Increase socket timeout from 2 to 20 seconds

--- parlo.py
+++ parlo.py
@@ -1,9 +1,10 @@
+#SI FA PARTIRE PER SECONDO
 import socket
 import sys
 messaggio = sys.argv[1]
 s = socket.socket()
-s.settimeout(2)
-indirizzo = '127.0.0.1'
+s.settimeout(20)
+indirizzo = '127.0.0.1' #QUI VA MESSO L'IP DEL COMPAGNO
 porta = 6364
 s.connect((indirizzo, porta))
 s.sendall(messaggio.encode())
